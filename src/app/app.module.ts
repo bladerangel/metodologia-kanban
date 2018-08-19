0ab8bafbded99b2434e04a987898213998b3b3a7
bladerangel <bladerangel10@gmail.com>
2018-08-19 18:40:50 -0300

    importando formulario reativo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgDragDropModule } from 'ng-drag-drop';
 
 import { QuadrosComponent } from './modulos/quadro/componentes/quadros/quadros.component';
-import { CabecalhoComponent } from './modulos/compartilhado/componentes/cabecalho/cabecalho.component';
 import { QuadroComponent } from './modulos/quadro/componentes/quadro/quadro.component';
 import { QuadroDetalheComponent } from './modulos/quadro/componentes/quadro-detalhe/quadro-detalhe.component';
-import { CaixaModalComponent } from './modulos/compartilhado/componentes/caixa-modal/caixa-modal.component';
 import { ListaComponent } from './modulos/lista/componentes/lista/lista.component';
 import { AtividadeComponent } from './modulos/atividade/componentes/atividade/atividade.component';
+import { CaixaModalComponent } from './modulos/compartilhado/caixa-modal/componentes/caixa-modal/caixa-modal.component';
+import { CabecalhoComponent } from './modulos/compartilhado/cabecalho/cabecalho.component';
+import { CaixaToastComponent } from './modulos/compartilhado/caixa-toast/components/caixa-toast/caixa-toast.component';
 
 
 @NgModule({
@@ -24,11 +26,13 @@ import { AtividadeComponent } from './modulos/atividade/componentes/atividade/at
     QuadroDetalheComponent,
     CaixaModalComponent,
     ListaComponent,
-    AtividadeComponent
+    AtividadeComponent,
+    CaixaToastComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
     NgDragDropModule.forRoot()
