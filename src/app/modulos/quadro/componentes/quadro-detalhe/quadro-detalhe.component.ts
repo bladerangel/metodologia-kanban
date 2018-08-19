1fdcc4c1e6c0f1e7d20fd7401df73d9129b1d0ba
bladerangel <bladerangel10@gmail.com>
2018-08-19 18:38:35 -0300

    adicionando nova lista

--- src/app/modulos/quadro/componentes/quadro-detalhe/quadro-detalhe.component.ts
+++ src/app/modulos/quadro/componentes/quadro-detalhe/quadro-detalhe.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { ListaService } from '../../../lista/servicos/lista.service';
+import { AtividadeService } from '../../../atividade/servicos/atividade.service';
+import { CaixaModalService } from '../../../compartilhado/caixa-modal/servicos/caixaModal.service';
 import { Lista } from '../../../lista/modelos/lista';
+import { Atividade } from '../../../atividade/modelos/atividade';
 
 
 @Component({
@@ -14,12 +17,16 @@ export class QuadroDetalheComponent implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private listaService: ListaService
+    private listaService: ListaService,
+    private atividadeService: AtividadeService,
+    private caixaModalService: CaixaModalService
   ) { }
 
   listas: Lista[];
+  quadroId: number;
 
   ngOnInit() {
+    this.quadroId = this.activatedRoute.snapshot.params['id'];
     this.atualizarListas();
   }
 
@@ -27,9 +34,30 @@ export class QuadroDetalheComponent implements OnInit {
     this.atualizarListas();
   }
 
+  gerenciarLista(dados: any) {
+    if (dados.modo == 'criacao') {
+      if (dados.componente == 'atividade') {
+        this.atividadeService.salvarAtividade(new Atividade(null, dados.formulario.nome, dados.formulario.descricao, dados.lista.quadroId, dados.lista.id))
+          .subscribe(atividade => this.atualizarListas());
+      } else {
+        this.listaService.salvarLista(new Lista(null, dados.formulario.nome, this.quadroId))
+          .subscribe(lista => this.atualizarListas());
+      }
+    }
+  }
+
+  adicionarLista() {
+    this.caixaModalService.emitirEvento({ modo: 'criacao', componente: 'lista', formulario: {} });
+  }
+
   atualizarListas() {
-    this.listaService.getLista(this.activatedRoute.snapshot.params['id'])
+    this.listaService.getListas(this.quadroId)
       .subscribe(listas => this.listas = listas);
+  }
+
+  removerLista(id: number) {
+    this.listas.splice(this.listas.findIndex((quadro) => quadro.id == id), 1);
+    this.listaService.removerLista(id).subscribe();
   }
 
 }
