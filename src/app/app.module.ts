d0edb44fd2e47ef95b2e97f6775270069c55cdf7
bladerangel <bladerangel10@gmail.com>
2018-08-21 15:34:29 -0300

    removendo arquivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './modulos/compartilhado/cabecalho/cabecalho.component';
-
 
 @NgModule({
   declarations: [
